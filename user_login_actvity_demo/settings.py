f2b7c1b9fb2d4cd84ff49208abc6c10cb9779864
Mageshwaran-Baskar <55185936+Mageshwaran-Baskar@users.noreply.github.com>
2020-09-29 10:50:14 +0530

    Update settings.py

--- user_login_actvity_demo/settings.py
+++ user_login_actvity_demo/settings.py
@@ -15,12 +15,14 @@ import os
 
 STATIC_ROOT = 'staticfiles'
 
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
